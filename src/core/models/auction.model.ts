76254f4bb01d7dce12ec9a9373afca6c6d635ac8
ArmNem <58069062+ArmNem@users.noreply.github.com>
2021-05-08 12:32:39 +0200

    CRUD in repos

--- src/core/models/auction.model.ts
+++ src/core/models/auction.model.ts
@@ -2,7 +2,7 @@ import { Bid } from "./bid.model";
 import { User } from "./user.model";
 
 export interface Auction{
-    id: string;
+    id?: string;
     name: string;
     description: string;
     startPrice: number;
